0fe99efda36ff5a182d71d8132ffd69cb4773a73
Viktor Rutberg <wishie@gmail.com>
2016-12-07 17:24:20 +0100

    Added solution for day 2.

---
+++ 2/lib.py
@@ -0,0 +1,105 @@
+#!/usr/local/bin/python3
+
+from enum import Enum, IntEnum
+
+class Direction(Enum):
+    up = 1
+    right = 2
+    down = 3
+    left = 4
+
+class DirectionFactory:
+    @staticmethod
+    def from_char(c: str):
+        if c not in 'LURD':
+            return NotImplemented
+
+        if c is 'L':
+            return Direction.left
+        elif c is 'U':
+            return Direction.up
+        elif c is 'R':
+            return Direction.right
+        elif c is 'D':
+            return Direction.down
+
+    @staticmethod
+    def from_string(s: str):
+        directions = []
+
+        for c in s:
+            directions.append(DirectionFactory.from_char(c))
+
+        return directions
+
+class KeypadPosition:
+    def __init__(self, x: int = 0, y: int = 0):
+        self.x = x
+        self.y = y
+
+    def move(self, direction: Direction):
+        if direction is Direction.up:
+            return KeypadPosition(self.x, max(-1, self.y - 1))
+        elif direction is Direction.right:
+            return KeypadPosition(min(1, self.x + 1), self.y)
+        elif direction is Direction.down:
+            return KeypadPosition(self.x, min(1, self.y + 1))
+        elif direction is Direction.left:
+            return KeypadPosition(max(-1, self.x - 1), self.y)
+
+    def to_digit(self):
+        if self.x is -1 and self.y is -1:
+            return 1
+        elif self.x is 0 and self.y is -1:
+            return 2
+        elif self.x is 1 and self.y is -1:
+            return 3
+        elif self.x is -1 and self.y is 0:
+            return 4
+        elif self.x is 0 and self.y is 0:
+            return 5
+        elif self.x is 1 and self.y is 0:
+            return 6
+        elif self.x is -1 and self.y is 1:
+            return 7
+        elif self.x is 0 and self.y is 1:
+            return 8
+        elif self.x is 1 and self.y is 1:
+            return 9
+
+    def __str__(self):
+        return "KeypadPosition(x: {}, y: {})".format(self.x, self.y)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class KeypadInstruction:
+    def __init__(self, directions):
+        self.directions = directions
+
+    def follow(self):
+        current_position = KeypadPosition()
+
+        for direction in self.directions:
+            current_position = current_position.move(direction)
+
+        return current_position.to_digit()
+
+class Keypad:
+    def __init__(self, instructions):
+        self.digits = []
+        self.instructions = instructions
+
+    def follow_instructions(self):
+        for instruction in self.instructions:
+            self.digits.append(instruction.follow())
---
+++ 2/tests.py
@@ -0,0 +1,77 @@
+#!/usr/local/bin/python3
+
+import unittest
+from lib import *
+
+class KeypadPositionTest(unittest.TestCase):
+
+    def test_move_left_from_2(self):
+        self.assertEqual(KeypadPosition(0, -1).move(Direction.left), KeypadPosition(-1, -1))
+
+    def test_move_left_from_1(self):
+        self.assertEqual(KeypadPosition(-1, -1).move(Direction.left), KeypadPosition(-1, -1))
+
+    def test_move_up(self):
+        self.assertEqual(KeypadPosition().move(Direction.up), KeypadPosition(0, -1))
+
+    def test_move_right(self):
+        self.assertEqual(KeypadPosition().move(Direction.right), KeypadPosition(1, 0))
+
+    def test_move_down(self):
+        self.assertEqual(KeypadPosition().move(Direction.down), KeypadPosition(0, 1))
+
+    def test_move_left(self):
+        self.assertEqual(KeypadPosition().move(Direction.left), KeypadPosition(-1, 0))
+
+    def test_move_up_is_capped(self):
+        self.assertEqual(KeypadPosition(0, -1).move(Direction.up), KeypadPosition(0, -1))
+
+    def test_move_right_is_capped(self):
+        self.assertEqual(KeypadPosition(1, 0).move(Direction.right), KeypadPosition(1, 0))
+
+    def test_move_down_is_capped(self):
+        self.assertEqual(KeypadPosition(0, 1).move(Direction.down), KeypadPosition(0, 1))
+
+    def test_move_left_is_capped(self):
+        self.assertEqual(KeypadPosition(-1, 0).move(Direction.left), KeypadPosition(-1, 0))
+
+    def test_to_digit(self):
+        self.assertEqual(KeypadPosition(-1, -1).to_digit(), 1)
+        self.assertEqual(KeypadPosition(0, -1).to_digit(), 2)
+        self.assertEqual(KeypadPosition(1, -1).to_digit(), 3)
+        self.assertEqual(KeypadPosition(-1, 0).to_digit(), 4)
+        self.assertEqual(KeypadPosition(0, 0).to_digit(), 5)
+        self.assertEqual(KeypadPosition(1, 0).to_digit(), 6)
+        self.assertEqual(KeypadPosition(-1, 1).to_digit(), 7)
+        self.assertEqual(KeypadPosition(0, 1).to_digit(), 8)
+        self.assertEqual(KeypadPosition(1, 1).to_digit(), 9)
+
+class KeypadInstructionTest(unittest.TestCase):
+    def test_example_one(self):
+        directions = [Direction.up, Direction.left, Direction.left]
+        self.assertEqual(KeypadInstruction(directions).follow(), 1)
+
+    def test_example_two(self):
+        directions = [Direction.right, Direction.right, Direction.down, Direction.down, Direction.down]
+        self.assertEqual(KeypadInstruction(directions).follow(), 9)
+
+    def test_example_three(self):
+        directions = [Direction.left, Direction.up, Direction.right, Direction.down, Direction.left]
+        self.assertEqual(KeypadInstruction(directions).follow(), 8)
+
+    def test_example_three(self):
+        directions = [Direction.up, Direction.up, Direction.up, Direction.up, Direction.down]
+        self.assertEqual(KeypadInstruction(directions).follow(), 5)
+
+class DirectionFactoryTests(unittest.TestCase):
+    def test_from_string(self):
+        self.assertEqual(DirectionFactory.from_string('LURD'), [Direction.left, Direction.up, Direction.right, Direction.down])
+
+    def test_from_char(self):
+        self.assertEqual(DirectionFactory.from_char('L'), Direction.left)
+
+    def test_from_char_unsupported_char(self):
+        self.assertEqual(DirectionFactory.from_char('X'), NotImplemented)
+
+if __name__ == '__main__':
+    unittest.main()
