5722b8d7bece3a7f1c819d3770804023f401aa50
Viktor Rutberg <wishie@gmail.com>
2016-12-07 22:37:15 +0100

    Updatd to a slightly more compact solution.

--- 3/solution-part-2.py
+++ 3/solution-part-2.py
@@ -4,18 +4,13 @@ if __name__ == '__main__':
 
     input = [s.split() for s in open('input.txt', 'r').read().strip().split('\n')]
 
-    triangles = []
     possibles = 0
 
-    for i in range(0, len(input), 3):
-        triangles.append([input[i][0], input[i+1][0], input[i+2][0]])
-        triangles.append([input[i][1], input[i+1][1], input[i+2][1]])
-        triangles.append([input[i][2], input[i+1][2], input[i+2][2]])
+    for row in range(0, len(input), 3):
+        for column in range(3):
+            triangle = sorted([int(s) for s in [input[row][column], input[row+1][column], input[row+2][column]]])
 
-    for triangle in triangles:
-        triangle = sorted(list(map(lambda s: int(s), triangle)))
-
-        if triangle[0] + triangle[1] > triangle[2]:
-            possibles += 1
+            if triangle[0] + triangle[1] > triangle[2]:
+                possibles += 1
 
     print(possibles)
