422de6d3632a6b2607995a9294b20f7e4910fe9a
Viktor Rutberg <wishie@gmail.com>
2016-12-08 13:56:13 +0100

    Cleaned up unused imports and other stuff.

--- 4/solution.py
+++ 4/solution.py
@@ -2,16 +2,11 @@
 
 from lib import *
 
-import operator
-import re
-
-
 if __name__ == '__main__':
 
     input = open('input.txt', 'r').read().strip().split('\n')
 
     sum = 0
-    regex = re.compile('((?:[a-z]+-)+)([0-9]+)\[([a-z]+)\]')
     calculator = ChecksumCalculator()
 
     for room_string in input:
