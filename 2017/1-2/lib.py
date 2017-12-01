4f2327ef31201674ef023cdf1f9695d05fc7788f
Viktor Rutberg <wishie@gmail.com>
2017-12-01 21:34:25 +0100

    Add oneliner solution to day 1 part 2

--- 2017/1-2/lib.py
+++ 2017/1-2/lib.py
@@ -5,9 +5,15 @@ from functools import reduce
 class Solver:
     def solve(self, input: str):
         result = 0
+        length = len(input)
 
         for i in enumerate(input):
-            if i[1] == input[(i[0] + len(input) // 2) % len(input)]:
+            if i[1] == input[(i[0] + length // 2) % length]:
                 result += int(i[1])
 
         return result
+
+    def solve_oneliner(self, input: str):
+        length = len(input)
+
+        return sum(int(c) for i, c in enumerate(input) if c == input[(i + length // 2) % length])
--- 2017/1-2/tests.py
+++ 2017/1-2/tests.py
@@ -19,5 +19,20 @@ class SolverTests(unittest.TestCase):
     def test_example_five(self):
         self.assertEqual(Solver().solve("12131415"), 4)
 
+    def test_example_one_oneliner(self):
+        self.assertEqual(Solver().solve_oneliner("1212"), 6)
+
+    def test_example_two_oneliner(self):
+        self.assertEqual(Solver().solve_oneliner("1221"), 0)
+
+    def test_example_three_oneliner(self):
+        self.assertEqual(Solver().solve_oneliner("123425"), 4)
+
+    def test_example_four_oneliner(self):
+        self.assertEqual(Solver().solve_oneliner("123123"), 12)
+
+    def test_example_five_oneliner(self):
+        self.assertEqual(Solver().solve_oneliner("12131415"), 4)
+
 if __name__ == '__main__':
     unittest.main()
