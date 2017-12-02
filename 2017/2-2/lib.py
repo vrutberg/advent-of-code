aa6986334385b13e859034667fa864a8848fbdc6
Viktor Rutberg <wishie@gmail.com>
2017-12-02 13:09:02 +0100

    More compact solution using itertools.product

--- 2017/2-2/lib.py
+++ 2017/2-2/lib.py
@@ -1,18 +1,14 @@
 #!/usr/local/bin/python3
 
-import sys
+import itertools
 
 class Solver:
     def solve(self, input: str):
         return sum([self.solve_row(row) for row in input])
 
     def solve_row(self, input: [int]):
-        for index, number in enumerate(input):
-            for otherIndex, otherNumber in enumerate(input):
-                if index == otherIndex:
-                    continue
+        for number, otherNumber in filter(lambda x: x[0] != x[1], itertools.product(input, input)):
+            division = int(number) / int(otherNumber)
 
-                division = int(number) / int(otherNumber)
-
-                if division % 1 == 0:
-                    return division
+            if division % 1 == 0:
+                return int(division)
