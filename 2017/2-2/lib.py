79904f5bb1925b070ae1d5d5476118ea34c89cad
Viktor Rutberg <wishie@gmail.com>
2017-12-02 13:16:04 +0100

    Perform type conversion before passing input into solve_row

--- 2017/2-2/lib.py
+++ 2017/2-2/lib.py
@@ -4,11 +4,11 @@ import itertools
 
 class Solver:
     def solve(self, input: str):
-        return sum([self.solve_row(row) for row in input])
+        return sum([self.solve_row([int(i) for i in row]) for row in input])
 
     def solve_row(self, input: [int]):
-        for number, otherNumber in filter(lambda x: x[0] != x[1], itertools.product(input, input)):
-            division = int(number) / int(otherNumber)
+        for number, otherNumber in filter(lambda x: x[0] > x[1], itertools.product(input, input)):
+            division = number / otherNumber
 
             if division % 1 == 0:
                 return int(division)
