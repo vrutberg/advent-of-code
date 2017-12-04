0498852189d51a7508a79b2a59d21ad7a4c27eeb
Viktor Rutberg <wishie@gmail.com>
2017-12-04 18:25:00 +0100

    Prepared for day 3 part 2

---
+++ 2017/3-2/solution.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+
+from lib import *
+
+if __name__ == '__main__':
+    print("solution: {}".format(Solver().solve(361527)))
---
+++ 2017/3-2/tests.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+from lib import *
+import unittest
+
+class SolverTests(unittest.TestCase):
+    def test_numbers_per_face(self):
+        self.assertEqual(1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
