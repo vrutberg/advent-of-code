e2deb7bd824ac01faad279fff5280f4e979f65ef
Viktor Rutberg <wishie@gmail.com>
2017-12-04 22:14:55 +0100

    More compact solution

--- 2017/4-2/solution.py
+++ 2017/4-2/solution.py
@@ -6,17 +6,8 @@ def solve(input: [str]):
     return sum([is_valid(passphrase) for passphrase in input])
 
 def is_valid(passphrase: str):
-    words = passphrase.split(" ")
-
-    if len(set(words)) != len(words):
-        return 0
-
-    sorted_words = [''.join(sorted(c)) for c in [word for word in words]]
-
-    if len(set(sorted_words)) != len(sorted_words):
-        return 0
-
-    return 1
+    sorted_words = [''.join(sorted(c)) for c in [word for word in passphrase.split(" ")]]
+    return 1 if len(set(sorted_words)) == len(sorted_words) else 0
 
 if __name__ == '__main__':
     data = open('./input.txt', 'r').read().strip().split("\n")
