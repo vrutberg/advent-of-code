6651f5ad617a68b214fb5841ff98a637a66e783a
Viktor Rutberg <wishie@gmail.com>
2017-12-04 18:18:52 +0100

    Add solution for day 3

--- 2017/3-1/lib.py
+++ 2017/3-1/lib.py
@@ -2,4 +2,52 @@
 
 class Solver:
     def solve(self, input: int):
-        return 0
+        corner = self.find_corner(361527)
+        target_corner = self.traverse(corner[0], corner[1])
+        return target_corner[0] + target_corner[1]
+
+    def find_corner(self, number):
+        if number == 1:
+            return 0
+
+        if number == 2 or number == 3:
+            return 1
+
+        currentNumber = 3
+        step = 8
+        offset = 2
+
+        i = 1
+        while True:
+            nextCurrentNumber = offset + currentNumber + (step * i)
+
+            if number >= currentNumber and number < nextCurrentNumber:
+                return ((i, i), number-currentNumber)
+
+            currentNumber = nextCurrentNumber
+            i += 1
+
+
+    def traverse(self, point, numbers):
+        per_face = self.numbers_per_face(point[1])
+        x = point[0]
+        y = point[1]
+
+        for i in range(0, numbers):
+            if i >= 0 and i < per_face - 1:
+                x -= 1
+
+            elif i >= per_face - 1 and i < (per_face -1) * 2:
+                y -= 1
+
+            elif i >= (per_face -1) * 2 and i < ((per_face -1) * 3) + 1:
+                x += 1
+
+            else:
+                y += 1
+
+        return (x, y)
+
+
+    def numbers_per_face(self, layer):
+        return layer * 2 + 1
--- 2017/3-1/tests.py
+++ 2017/3-1/tests.py
@@ -4,17 +4,24 @@ from lib import *
 import unittest
 
 class SolverTests(unittest.TestCase):
-    def test_example_one(self):
-        self.assertEqual(Solver().solve(1), 0)
+    def test_numbers_per_face(self):
+        self.assertEqual(Solver().numbers_per_face(0), 1)
+        self.assertEqual(Solver().numbers_per_face(1), 3)
+        self.assertEqual(Solver().numbers_per_face(2), 5)
+        self.assertEqual(Solver().numbers_per_face(3), 7)
+        self.assertEqual(Solver().numbers_per_face(4), 9)
 
-    def test_example_two(self):
-        self.assertEqual(Solver().solve(12), 3)
+    def test_find_corner(self):
+        self.assertEqual(Solver().find_corner(17), ((2, 2), 4))
+        self.assertEqual(Solver().find_corner(13), ((2, 2), 0))
+        self.assertEqual(Solver().find_corner(30), ((2, 2), 17))
+        self.assertEqual(Solver().find_corner(31), ((3, 3), 0))
+        self.assertEqual(Solver().find_corner(55), ((3, 3), 24))
 
-    def test_example_three(self):
-        self.assertEqual(Solver().solve(23), 2)
-
-    def test_example_four(self):
-        self.assertEqual(Solver().solve(1024), 31)
+    def test_traverse(self):
+        self.assertEqual(Solver().traverse((3, 3), 24), (4, 2))
+        self.assertEqual(Solver().traverse((3, 3), 0), (3, 3))
+        self.assertEqual(Solver().traverse((2, 2), 17), (3, 2))
 
 if __name__ == '__main__':
     unittest.main()
