524929565fdbf55533907e6439fd7c7e3a61668e
Viktor Rutberg <wishie@gmail.com>
2017-12-05 20:36:19 +0100

    Add solution for day 5 part 2, albeit a sloooow one

---
+++ 2017/5-2/solution.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+def solve(input):
+    cursor = 0
+    iterations = 0
+
+    while cursor >= 0 and cursor <= len(input) - 1:
+        old_cursor = cursor
+        cursor += input[cursor]
+        input[old_cursor] += -1 if input[old_cursor] >= 3 else 1
+
+        iterations += 1
+
+    return iterations
+
+if __name__ == '__main__':
+    input = [int(str) for str in open('./input.txt', 'r').read().strip().split("\n")]
+    print("solution: {}".format(solve(input)))
