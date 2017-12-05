4fe733b6af0001bf569ddf39da1ef5a898c31d4f
Viktor Rutberg <wishie@gmail.com>
2017-12-05 20:21:57 +0100

    Move condition into the while statement

--- 2017/5-1/solution.py
+++ 2017/5-1/solution.py
@@ -4,15 +4,14 @@ def solve(input):
     cursor = 0
     iterations = 0
 
-    while True:
-        if cursor < 0 or cursor > len(input) - 1:
-            return iterations
-
+    while cursor >= 0 and cursor <= len(input) - 1:
         old_cursor = cursor
         cursor += input[cursor]
         input[old_cursor] += 1
 
         iterations += 1
+
+    return iterations
 
 if __name__ == '__main__':
     input = [int(str) for str in open('./input.txt', 'r').read().strip().split("\n")]
