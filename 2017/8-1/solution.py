415feb78352b37ae4f7f6450cc302cd789072018
Viktor Rutberg <wishie@gmail.com>
2017-12-10 00:02:03 +0100

    Add solution for day 8 part 1

---
+++ 2017/8-1/solution.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+
+from collections import defaultdict
+
+register = defaultdict(lambda: 0)
+comparators = {
+    '>=': lambda x, y: x >= y,
+    '!=': lambda x, y: x != y,
+    '==': lambda x, y: x == y,
+    '<=': lambda x, y: x <= y,
+    '<': lambda x, y: x < y,
+    '>': lambda x, y: x > y
+}
+
+def solve(input):
+    for instruction in input:
+        tokens = instruction.split(" ")
+
+        token_to_modify = tokens[0]
+        token_operation = tokens[1]
+        token_new_value = int(tokens[2])
+        token_condition_register = tokens[4]
+        token_condition_comparator = tokens[5]
+        token_condition_value = int(tokens[6])
+
+        if comparators[token_condition_comparator](register[token_condition_register], token_condition_value):
+            if token_operation == 'inc':
+                register[token_to_modify] += token_new_value
+            else:
+                register[token_to_modify] -= token_new_value
+
+    return sorted(register.values())[-1]
+
+if __name__ == '__main__':
+    input = open('./input.txt', 'r').read().strip().split("\n")
+    print("solution: {}".format(solve(input)))
