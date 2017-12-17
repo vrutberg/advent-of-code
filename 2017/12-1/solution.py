4d8374458ab1b449e867f67b8efc9dbb3250bbf2
Viktor Rutberg <wishie@gmail.com>
2017-12-17 14:47:27 +0100

    Add solution for day 12 part 1

---
+++ 2017/12-1/solution.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+
+def solve(input, needle):
+    nodes = {}
+    programs = set()
+
+    for line in input:
+        segments = line.split(" <-> ")
+        value = int(segments[0])
+        children = [int(i) for i in segments[1].split(", ")]
+        nodes[value] = children
+
+    programs.add(needle)
+    seen = set()
+
+    def p(needle):
+        programs.add(needle)
+
+        if len(seen) == len(nodes):
+            return
+
+        for x in nodes[needle]:
+            if x not in programs:
+                p(x)
+
+    p(needle)
+
+    return len(programs)
+
+if __name__ == '__main__':
+    input = open('./input.txt', 'r').read().strip().split("\n")
+    print("solution: {}".format(solve(input, 0)))
---
+++ 2017/12-1/tests.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+from solution import *
+import unittest
+
+class Tests(unittest.TestCase):
+    def test_example_one(self):
+        self.assertEqual(1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
