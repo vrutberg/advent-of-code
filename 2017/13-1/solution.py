919197e28cd91dab7f0451fe64d3277e17927cae
Viktor Rutberg <wishie@gmail.com>
2017-12-20 20:48:38 +0100

    Simplify solution by not using a Scanner class that moves its cursor

--- 2017/13-1/solution.py
+++ 2017/13-1/solution.py
@@ -1,46 +1,18 @@
 #!/usr/local/bin/python3
 
-from collections import defaultdict
-
-class Scanner:
-    position = 0
-    _moving_up = True
-
-    def __init__(self, size):
-        self._size = size
-
-    def move(self):
-        if self._moving_up:
-            if self.position == self._size - 1:
-                self.position -= 1
-                self._moving_up = False
-            else:
-                self.position += 1
-
-        else:
-            if self.position == 0:
-                self.position += 1
-                self._moving_up = True
-            else:
-                self.position -= 1
-
 def solve(input):
-    scanners = defaultdict(lambda: None)
+    scanners = dict()
 
     for value in input:
-        scanners.update({ int(value[0]): Scanner(int(value[1]))})
+        scanners[int(value[0])] = int(value[1])
 
     severity = 0
 
-    for pos in range(max(list(scanners.keys()))+1):
-        if scanners[pos] is not None and scanners[pos].position == 0:
-            severity += pos * scanners[pos]._size
+    for key in scanners:
+        value = scanners[key]
 
-        for p in scanners:
-            if scanners[p] is not None:
-                scanners[p].move()
-
-        pos += 1
+        if key % ((value - 1) * 2) == 0:
+            severity += key * value
 
     return severity
 
