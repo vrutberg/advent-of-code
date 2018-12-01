4f65ba5ae5085c8dfee6f6391c8f72d686bdb683
Viktor Rutberg <wishie@gmail.com>
2018-12-01 19:08:43 +0100

    Add timing information

--- 2018/01-2/Puzzle.playground/Contents.swift
+++ 2018/01-2/Puzzle.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 import PlaygroundSupport
 
+let start = Date()
+
 guard let fileUrl = Bundle.main.url(forResource: "input", withExtension: "txt"),
     let input = try? String.init(contentsOf: fileUrl, encoding: .utf8)else {
     fatalError()
@@ -14,12 +16,10 @@ var currentFrequency = 0
 while !frequencies.contains(currentFrequency) {
     frequencies.insert(currentFrequency)
     currentFrequency += changes[cursor]
-
-    if cursor == changes.count-1 {
-        cursor = 0
-    } else {
-        cursor += 1
-    }
+    cursor = (cursor == changes.count-1) ? 0 : cursor+1
 }
 
-print(currentFrequency)
+print("result: \(currentFrequency)")
+
+let end = Date()
+print("took: \(end.timeIntervalSince1970 - start.timeIntervalSince1970) seconds")
