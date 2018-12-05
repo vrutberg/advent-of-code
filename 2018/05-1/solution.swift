41d333c8625a45e6284644bca7c1a7d41ccb35cf
Viktor Rutberg <wishie@gmail.com>
2018-12-05 22:19:39 +0100

    Improve solution for day 5 part 1

--- 2018/05-1/solution.swift
+++ 2018/05-1/solution.swift
@@ -4,31 +4,35 @@ import Foundation
 
 let start = Date()
 
-var polymer = try! String(contentsOfFile: "./input.txt", encoding: .utf8)
+var compactedPolymer = ""
+
+try! String(contentsOfFile: "./input.txt", encoding: .utf8)
     .trimmingCharacters(in: .whitespacesAndNewlines)
+    .enumerated()
+    .forEach { index, char in
+        guard index > 0 else {
+            compactedPolymer.append(char)
+            return
+        }
 
-let triggers = "abcdefghijklmnopqrstuvwxyz".map { (c: Character) -> String in
-    [String(c), String(c).uppercased()].joined()
-} +  "abcdefghijklmnopqrstuvwxyz".map { (c: Character) -> String in
-    [String(c).uppercased(), String(c)].joined()
-}
+        var didRemoveAnything = false
 
-while(true) {
-    var performedAnyDeletions = false
+        if let lastChar = compactedPolymer.last {
+            let uppercaseChar = Character(UnicodeScalar(char.unicodeScalars.first!.value - 32)!)
+            let lowercaseChar = Character(UnicodeScalar(char.unicodeScalars.first!.value + 32)!)
 
-    for trigger in triggers {
-        if let range = polymer.range(of: trigger) {
-            polymer.removeSubrange(range)
-            performedAnyDeletions = true
+            if lastChar == uppercaseChar || lastChar == lowercaseChar {
+                _ = compactedPolymer.popLast()
+                didRemoveAnything = true
+            }
+        }
+
+        if !didRemoveAnything {
+            compactedPolymer.append(char)
         }
     }
 
-    if !performedAnyDeletions {
-        break
-    }
-}
-
-print(polymer.count)
+print(compactedPolymer.count)
 
 let end = Date()
 print("took: \(end.timeIntervalSince1970 - start.timeIntervalSince1970) seconds")
